e13cf76e12f63cddc84ff490616d225b2b3b4457
mechamaster-club <105486743+mechamaster-club@users.noreply.github.com>
2022-07-05 14:54:21 +0800

    change import Member path

--- token.sol
+++ token.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../Manager/Member.sol";
+import "./Member.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
